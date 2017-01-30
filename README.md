fb578b434f893894c0f1b53b498bb16646154e3c
Mike Oliver <me@mikeoliver.com>
2017-01-29 19:37:04 -0500

    Upload component
    Push component up

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # vue2-star-rating
-Vue 2 component for star ratings
+A simple Vue 2 component for star ratings that does not rely on any plugins or dependent js.
+
+This vue component is based on [https://github.com/Jamiek94/Vuejs-rating](https://github.com/Jamiek94/Vuejs-rating)
+
+How to Use:  import into your javascript:
+
+```javascript
+import showRatings from './components/showRatings.vue';
+...
+Vue.component('show-ratings',showRatings);
+...
+var vm = new Vue({
+  el: '#employee',
+	components: {
+    ...,
+    showRatings
+  }
+});
+```
+
+Reference the component wherever you need a rating based on stars:
+```php
+<div id="employee">
+  <show-ratings max="10" v-model="question.value"></show-ratings>
+</div>
+```
---
+++ showRatings.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="star-rating">
+       <label
+         class="star-rating__star"
+         v-for="rating in ratings"
+         :class="{ 'is-selected' : ((value >= rating) && value != null), 'is-disabled': disabled}"
+         v-on:mouseover="star_over(rating)"
+         v-on:mouseout="star_out"
+         v-on:click.prevent="set(rating)">
+       <input ref="input" class="star-rating star-rating__checkbox" type="radio" v-bind:value="value" v-on:input="updateValue">★</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+          max: '',
+          disabled: ''
+        },
+
+        data : function () {
+          return {
+            value: 0
+          };
+        },
+
+        watch: {
+          'value': function (value) {
+            this.$emit('input', value)
+          }
+        },
+
+        computed: {
+            ratings : function() {
+                if(!this.max) { return [1, 2, 3, 4, 5]; }
+                var numberArray = [];
+                for(var i = 1; this.max >= i; i++){
+                    numberArray.push(i);
+                }
+                return numberArray;
+            }
+        },
+
+        methods: {
+            updateValue: function (value) {
+                this.$emit('input',value);
+            },
+
+            star_over: function (index) {
+                if (this.disabled) {
+                    return;
+                }
+
+                this.temp_value = this.value;
+                this.value = index;
+            },
+            star_out: function () {
+                if (this.disabled) {
+                    return;
+                }
+
+                this.value = this.temp_value;
+            },
+            set: function (value) {
+                if (this.disabled) {
+                    return;
+                }
+
+                this.temp_value = value;
+                this.value = value;
+            }
+        }
+    }
+</script>
+
+<style>
+    .star-rating .star-rating__star {
+      display: inline-block;
+      padding: 3px;
+      vertical-align: middle;
+      line-height: 1;
+      font-size: 1.1em;
+      color: #ABABAB;
+      transition: color .2s ease-out;
+      cursor: pointer;
+    }
+    .star-rating input[type="radio"]{
+      display: none;
+    }
+    .star-rating .star-rating__star.is-selected {
+      color: #FFD700;
+    }
+</style>
